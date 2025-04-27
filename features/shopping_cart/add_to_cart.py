034b0b94b1540eddf0c7c44ba2a78e6d39668650
@erick <erickescribaa@gmail.com>
2025-04-27 02:01:51 -0500

    feat(add_to_cart): implement add_to_cart logic

--- features/shopping_cart/add_to_cart.py
+++ features/shopping_cart/add_to_cart.py
@@ -1,27 +1,41 @@
 import os
+from errors.custom_exception import CustomException
 from errors.invalid_option import invalid_option
-from errors.invalid_quantity import invalid_quantity
-from features.shopping_cart.list_products import list_products
-from features.shopping_cart.list_products import loads_products_data
+from services.cart import loads_cart_data, save_cart_data
+from services.products import loads_products_data
+from utils.highlight import highlight
 
 
 def add_to_cart() -> None:
     products_data = loads_products_data()
     print("Agregar al carrito")
-    product_code = input("Ingrese código de producto: ")
-    if product_code.upper() not in products_data:
-        os.system("clear")
-        print("ERROR: Código de producto no existe")
-        add_to_cart()
     try:
+        product_code = input("Ingrese código de producto: ").upper()
+
+        if product_code not in products_data:
+            raise CustomException("ERROR: Código de producto no existe")
+
         quantity = int(input("Cantidad: "))
-        # FIXME: raise error if quantity is negative
+
         if quantity <= 0:
-            invalid_quantity()
-            # FIXME: resume quantity input if invalid input
-            add_to_cart()
+            raise CustomException("ERROR: Cantidad no puede ser 0 o negativo")
+
     except ValueError:
         invalid_option()
-        add_to_cart()
+    except CustomException as e:
+        print(highlight(f'\n{e}\n', color="yellow", bold=True))
+    else:
+        os.system("clear")
+        cart_data = loads_cart_data()
+        if (cart_data.get(product_code)):
+            cart_data[product_code]['quantity'] += quantity
+            print("\n✅ Se actualizo la cantidad del producto en el carrito.\n")
+        else:
+            cart_data[product_code] = {
+                "name": products_data[product_code]["name"],
+                "price": products_data[product_code]["price"],
+                "quantity": quantity
+            }
+            print("\n✅ Producto agregado al carrito.\n")
 
-    print(f"✅ Producto agregado al carrito.")
+        save_cart_data(cart_data)
