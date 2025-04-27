e44a3bcda6dd61a78701acecae1ba8b2e0240238
@erick <erickescribaa@gmail.com>
2025-04-27 02:07:15 -0500

    chore: integrade features

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import os
 from typing import Callable
+
 from errors.invalid_option import invalid_option
+
 from features.menu import show_menu
+from features.shopping_cart.checkout import checkout
+from features.shopping_cart.clean_cart import clean_cart
+from features.shopping_cart.remove_from_cart import remove_from_cart
+from features.shopping_cart.show_cart import show_cart
 from features.shopping_cart.add_to_cart import add_to_cart
 from features.shopping_cart.list_products import list_products
 
+from utils.highlight import highlight
 
-def add_and_menu() -> None:
-    add_to_cart()
-    main()
+
+def cart_and_menu() -> None:
+    os.system("clear")
+    show_cart()
 
 
 def list_and_menu() -> None:
     os.system("clear")
     list_products()
-    main()
 
 
 def exit_program() -> None:
@@ -25,30 +32,33 @@ def exit_program() -> None:
 def default_action() -> None:
     os.system("clear")
     invalid_option()
-    main()
 
 
 def handle_action(action: int) -> None:
     options: dict[int, Callable[[], None]] = {
         1: list_and_menu,
-        2: add_and_menu,
-        3: default_action,
-        4: default_action,
-        5: default_action,
-        6: default_action,
+        2: add_to_cart,
+        3: remove_from_cart,
+        4: clean_cart,
+        5: cart_and_menu,
+        6: checkout,
         7: exit_program
     }
     options.get(action, default_action)()
 
 
 def main():
-    print("\nBienvenido a la tienda virtual 🛍️\n")
-    show_menu()
-    try:
-        action: int = int(input("¿Qué deseas hacer? (1-7): "))
-        handle_action(action)
-    except ValueError:
-        default_action()
+    while True:
+        print(highlight("\nBienvenido a la tienda virtual 🛍️\n",
+              color="white", bold=True))
+        show_menu()
+        try:
+            action: int = int(input("¿Qué deseas hacer? (1-7): "))
+            handle_action(action)
+        except ValueError:
+            default_action()
+        except KeyboardInterrupt:
+            exit_program()
 
 
 main()
